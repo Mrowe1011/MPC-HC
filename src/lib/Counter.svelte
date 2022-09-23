dcb2530f45cc839bcb7b075e0f05ea68aa36a04c
Kevin Bahner <kbahner@kent.edu>
2022-09-23 13:12:33 -0400

    init

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,61 @@
+<script>
+var filePath;
+   var curPos;
+   var len;
+   var state;
+   var pbr;
+   var eta;
+   var volume;
+   var muted; // 1 no sound
+   var startTime = new Date().getTime();
+   var sliderSize = 500;
+   var sliderButtonWidth = 15;
+   var vsb = 10;
+   var vss = 100;
+   var vs;
+   var sc = 0;
+   var rdirt;
+   var Live;
+   var re;
+   var sas;
+   var cpf;
+   var cp;
+   var s;
+   var m;
+   var sb1;
+   var sb2;
+   var sb3;
+   var vs1;
+   var vs2;
+   var vs3;
+   var etaup = false;
+   var httpRequestStatus;
+   
+</script>
+
+<span>{curPos}</span>
+<span>{len}</span>
+<span>{state}</span>
+<span>{pbr}</span>
+<span>{curPos}</span>
+<span>{eta}</span>
+<span>{volume}</span>
+<span>{muted}</span>
+<span>{curPos}</span>
+<span>{startTime}</span>
+<span>{vs}</span>
+<span>{rdirt}</span>
+<span>{Live}</span>
+<span>{re}</span>
+<span>{sas}</span>
+<span>{cpf}</span>
+<span>{cp}</span>
+<span>{s}</span>
+<span>{m}</span>
+<span>{sb1}</span>
+<span>{sb2}</span>
+<span>{sb3}</span>
+<span>{vs1}</span>
+<span>{vs2}</span>
+<span>{vs3}</span>
+<span>{httpRequestStatus}</span>
